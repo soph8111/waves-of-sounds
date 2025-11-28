789251b43a6ed5990b52f72e052e0a46e45347dc
Sophie Spang <soph8111@stud.kea.dk>
2025-11-28 12:39:53 +0100

    .env production/development

--- waves-of-sounds-client/src/api.ts
+++ waves-of-sounds-client/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL || "https://waves-of-sounds-server.onrender.com";
+const API_BASE = import.meta.env.VITE_API_URL || "https://waves-of-sounds-server.onrender.com";
 const api = axios.create({ baseURL: API_BASE });
 
 export default api;
