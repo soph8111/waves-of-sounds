cd6ad6922c234db4f5f932ab8f8a11962c39a001
Sophie Spang <soph8111@stud.kea.dk>
2025-12-01 15:18:54 +0100

    Close menu on navigation to new page

--- waves-of-sounds-client/src/components/NavBar.tsx
+++ waves-of-sounds-client/src/components/NavBar.tsx
@@ -9,8 +9,7 @@ import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 export default function Navbar() {
 
   const [ isOpen, setIsOpen ] = useState(false);
-
-  // Function for toggle menu
+  const closeMenu = () => setIsOpen(false);
   const toggleMenu = () => {
     setIsOpen((open) => !open) 
   }
@@ -27,187 +26,35 @@ export default function Navbar() {
         <img src="/img/other/logo.png" alt="Logo" />
       </Link>
       <ul className={isOpen ? 'is_open' : ''}>
-        <CustomLink to="./program">program</CustomLink>
-        <CustomLink to="./news">news</CustomLink>
-        <CustomLink to="./volunteers">volunteers</CustomLink>
-        <CustomLink to="./about">about</CustomLink>
+        <CustomLink to="./program" onClick={closeMenu}>program</CustomLink>
+        <CustomLink to="./news" onClick={closeMenu}>news</CustomLink>
+        <CustomLink to="./volunteers" onClick={closeMenu}>volunteers</CustomLink>
+        <CustomLink to="./about" onClick={closeMenu}>about</CustomLink>
       </ul>
       <button className="buy_ticket styled_button">Buy ticket</button>
     </nav>
   )
 }
 
-function CustomLink({ to, children, ...props }: { to: string, children: string }) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+function CustomLink({
+  to,
+  children,
+  onClick,
+  ...props
+}: {
+  readonly to: string;
+  readonly children: string;
+  readonly onClick?: () => void;
+}) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
   return (
     <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
+      <Link to={to} onClick={onClick} {...props}>
         {children}
       </Link>
     </li>
-  )
+  );
 }
 
-
-
-// /* Navbar from Chakra: https://chakra-templates.vercel.app/navigation/navbar*/
-
-// import {
-//     Box,
-//     Flex,
-//     Text,
-//     IconButton,
-//     Button,
-//     Stack,
-//     Collapse,
-//     useColorModeValue,
-//     useDisclosure,
-//   } from '@chakra-ui/react'
-  
-//   import {
-//     HamburgerIcon,
-//     CloseIcon,
-//   } from '@chakra-ui/icons'
-
-//   interface NavItem {
-//     label: string
-//     href?: string
-//   }
-  
-//   const NAV_ITEMS: Array<NavItem> = [
-//     {
-//       label: 'program',
-//       href: '../pages/Program.tsx',
-//     },
-//     {
-//       label: 'news',
-//       href: '#',
-//     },
-//     {
-//       label: 'volunteers',
-//       href: '#',
-//     },
-//     {
-//       label: 'about',
-//       href: '#',
-//     },
-//   ]
-  
-//   export default function NavBar() {
-//     const { isOpen, onToggle } = useDisclosure()
-  
-//     return (
-//       <Box>
-//         <Flex
-//           bgColor={'rgb(143, 143, 143, 0.4)'}
-//           position={'fixed'}
-//           maxH={'60px'}
-//           width={'100%'}
-//           align={'center'}
-//           py={{ base: 2 }}
-//           px={{ base: 4 }}
-//           >
-//           <Flex
-//             flex={{ base: 1, md: 'auto' }}
-//             display={{ base: 'flex', md: 'none' }}>
-//             <IconButton
-//               onClick={onToggle}
-//               icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-//               variant={'ghost'}
-//               aria-label={'Toggle Navigation'}
-//             />
-//           </Flex>
-//           <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-//             <a href="../src/App.tsx">logo
-//               {/* <img src="/img/other/logo.png" alt="logo"/> */}
-//             </a>
-//             <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
-//               <DesktopNav />
-//             </Flex>
-//           </Flex>
-  
-//           <Stack
-//             flex={{ base: 1, md: 0 }}
-//             justify={'flex-end'}
-//             direction={'row'}
-//             spacing={6}>
-//             <Button 
-//             as={'a'} 
-//             href={'#'}
-//             bgColor={'#083C6E'}
-//             color={'white'}
-//             >
-//               Buy ticket
-//             </Button>
-//           </Stack>
-//         </Flex>
-  
-//         <Collapse in={isOpen} >
-//           <MobileNav />
-//         </Collapse>
-//       </Box>
-//     )
-//   }
-  
-//   const DesktopNav = () => {
-//     const linkColor = useColorModeValue('gray.600', 'gray.200')
-//     return (
-//       <Stack direction={'row'} spacing={4}>
-//         {NAV_ITEMS.map((navItem) => (
-//           <Box key={navItem.label}>
-//                 <Box
-//                   as="a"
-//                   p={2}
-//                   href={navItem.href ?? '#'}
-//                   fontSize={'sm'}
-//                   fontWeight={500}
-//                   color={linkColor}
-//                   _hover={{
-//                     textDecoration: 'none',
-//                   }}>
-//                   {navItem.label}
-//                 </Box>
-//           </Box>
-//         ))}
-//       </Stack>
-//     )
-//   }
-  
-//   const MobileNav = () => {
-//     return (
-//       <Stack 
-//       bgColor={'rgb(143, 143, 143, 0.4)'}
-//       p={4} 
-//       position={'absolute'}
-//       top={'60px'}
-//       width={'100%'}
-//       >
-//         {NAV_ITEMS.map((navItem) => (
-//           <MobileNavItem key={navItem.label} {...navItem} />
-//         ))}
-//       </Stack>
-//     )
-//   }
-  
-//   const MobileNavItem = ({ label, href }: NavItem) => {
-  
-//     return (
-//       <Stack spacing={4} >
-//         <Box
-//           py={2}
-//           as="a"
-//           href={href ?? '#'}
-//           justifyContent="space-between"
-//           alignItems="center"
-//           _hover={{
-//             textDecoration: 'none',
-//           }}>
-//           <Text fontWeight={600} color={useColorModeValue('gray.600', 'gray.200')}>
-//             {label}
-//           </Text>
-//         </Box>
-//       </Stack>
-//     )
-//   }
